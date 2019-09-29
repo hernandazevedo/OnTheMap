3257ab2ec582d19e36a5eece28b28ba7ba50351e
Hernand Azevedo <hernand.azevedo@gmail.com>
2019-09-29 11:17:03 -0300

    Initial Commit

---
+++ OnTheMap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OnTheMap
+//
+//  Created by Hernand Azevedo on 29/09/19.
+//  Copyright © 2019 Hernand Azevedo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
